319ba6c0261c2fa1874fd6a6c43b4a0c17abee00
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2024-12-18 01:59:09 +0300

    fix: function newCard->createCard, newCardsList -> createCardsList new variables in function createCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // @todo: Темплейт карточки
-function newCardsList() {
+function createCardsList() {
   initialCards.forEach(function(item) {
-    newCard(item['name'], item['link']);
+    cardList.append(createCard(item['name'], item['link'], deleteCard));
   });
 }
 
@@ -12,16 +12,22 @@ const cardList = content.querySelector('.places__list');
 
 //Чтобы получить содержимое template, нужно обратиться к его свойству content
 const addCard = document.querySelector('#card-template').content;
-newCardsList();
+createCardsList();
 
 // @todo: Функция создания карточки
-function newCard(name, link) {
+function createCard(name, link, deleteCard) {
   const newCard = addCard.querySelector('li').cloneNode(true);
-  newCard.querySelector('.card__image').setAttribute('src', link);
-  newCard.querySelector('.card__image').setAttribute('alt', name);
-  newCard.querySelector('.card__title').textContent = name;
-  newCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-  showCards(newCard);
+
+  const cardImg = newCard.querySelector('.card__image');
+  cardImg.src = link;
+  cardImg.alt = name;
+
+  const cardTitle = newCard.querySelector('.card__title');
+  cardTitle.textContent = name;
+
+  const deleteButton = newCard.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+  return newCard;
 }
 
 
@@ -33,16 +39,3 @@ function deleteCard(event) {
 }
 
 
-// @todo: Вывести карточки на страницу
-function showCards(newCard) {
-  //console.log("Проверяем на наличие в списке данной карточки");
-  const currentCardsPseudo = cardList.querySelectorAll('li');
-  const currentCards = Array.from(currentCardsPseudo);
-  const isCurrentShow = currentCards.find(function (item) {
-    return (item.querySelector('img').src === newCard.querySelector('img').src) && (item.querySelector('.card__title').textContent === newCard.querySelector('.card__title').textContent);
-  });
-  if(!isCurrentShow) {
-    //Карточки нет в списке! Добавляем его;
-    cardList.append(newCard);
-  }
-}
